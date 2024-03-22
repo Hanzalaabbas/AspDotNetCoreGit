2ddcc3650da0b735b0143ba0ab0b059cec8ac4ba
it-Hanzala <Windows@HANZALA>
2024-03-22 14:03:23 +0500

    Added ViewData , ViewData Attribute and Dynamic View

--- AspMVCCoreGit/Controllers/HomeController.cs
+++ AspMVCCoreGit/Controllers/HomeController.cs
@@ -1,25 +1,38 @@
 using AspMVCCoreGit.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Dynamic;
 
 namespace AspMVCCoreGit.Controllers
 {
     public class HomeController : Controller
     {
+        
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
+        //****************Code Start for [ViewData] Attribut****************
+        [ViewData]
+        public string? Title { get; set; }
+        //****************Code End for [ViewData] Attribut****************
         public IActionResult Index()
         {
+
+            Title = "Home from Controller to Layout USing ViewData Attributes";
             return View();
         }
 
         public IActionResult Privacy()
         {
+            //****************Code Start for Dynamic View****************
+            //dynamic data = new System.Dynamic.ExpandoObject(); 
+            //data.Title = Title;
+            //data.Name = Title;
+            //return View(data);
+            //****************Code Start for Dynamic View****************
             return View();
         }
 
