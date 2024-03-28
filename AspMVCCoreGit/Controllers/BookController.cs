6c07ff5c469ab862857342760ca81da0e063565d
it-Hanzala <Windows@HANZALA>
2024-03-28 10:57:42 +0500

    Added Form For AddNewBook

---
+++ AspMVCCoreGit/Controllers/BookController.cs
@@ -0,0 +1,18 @@
+﻿using AspMVCCoreGit.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspMVCCoreGit.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult AddNewBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddNewBook(BookModel bookModel)
+        {
+            return View();
+        }
+    }
+}
